b617fe5d3ba6f636cfb8321dfe350356a4f1e941
Jed Borseth <jedborseth@Jeds-MacBook-Pro.local>
2024-10-11 15:58:01 -0700

    commented out search page as it is not done:

--- src/app/dashboard/search/page.tsx
+++ src/app/dashboard/search/page.tsx
@@ -1,74 +1,82 @@
-"use client";
+// "use client";
 
-import algoliasearch from "algoliasearch/lite";
-import { Hit as AlgoliaHit } from "instantsearch.js";
-import React from "react";
-import {
-  Hits,
-  Highlight,
-  SearchBox,
-  RefinementList,
-  DynamicWidgets,
-} from "react-instantsearch";
-import { InstantSearchNext } from "react-instantsearch-nextjs";
+// import algoliasearch from "algoliasearch/lite";
+// import { Hit as AlgoliaHit } from "instantsearch.js";
+// import React from "react";
+// import {
+//   Hits,
+//   Highlight,
+//   SearchBox,
+//   RefinementList,
+//   DynamicWidgets,
+// } from "react-instantsearch";
+// import { InstantSearchNext } from "react-instantsearch-nextjs";
 
-const client = algoliasearch("KH60NZV6Q6", "2c44fcbda3712ff9731a7d9037c95f32");
+// const client = algoliasearch("KH60NZV6Q6", "2c44fcbda3712ff9731a7d9037c95f32");
 
-type HitProps = {
-  hit: AlgoliaHit<{
-    width: number;
-    length: number;
-    amount: number;
-  }>;
-};
+// type HitProps = {
+//   hit: AlgoliaHit<{
+//     width: number;
+//     length: number;
+//     amount: number;
+//   }>;
+// };
 
-function Hit({ hit }: HitProps) {
-  return (
-    <>
-      <Highlight hit={hit} attribute="name" className="Hit-label" />
-      <span className="Hit-price">${hit.amount}</span>
-    </>
-  );
-}
+// function Hit({ hit }: HitProps) {
+//   return (
+//     <>
+//       <Highlight hit={hit} attribute="name" className="Hit-label" />
+//       <span className="Hit-price">${hit.amount}</span>
+//     </>
+//   );
+// }
+
+// export default function Search() {
+//   return (
+//     <InstantSearchNext searchClient={client} indexName="instant_search" routing>
+//       <div className="Container">
+//         <div>
+//           <DynamicWidgets fallbackComponent={FallbackComponent} />
+//         </div>
+//         <div>
+//           <SearchBox />
+//           <Hits hitComponent={Hit} />
+//         </div>
+//       </div>
+//     </InstantSearchNext>
+//   );
+// }
+
+// function FallbackComponent({ attribute }: { attribute: string }) {
+//   return (
+//     <Panel header={attribute}>
+//       <RefinementList attribute={attribute} />
+//     </Panel>
+//   );
+// }
+
+// export function Panel({
+//   children,
+//   header,
+//   footer,
+// }: {
+//   children: React.ReactNode;
+//   header?: React.ReactNode;
+//   footer?: React.ReactNode;
+// }) {
+//   return (
+//     <div className="ais-Panel">
+//       {header && <div className="ais-Panel-header">{header}</div>}
+//       <div className="ais-Panel-body">{children}</div>
+//       {footer && <div className="ais-Panel-footer">{footer}</div>}
+//     </div>
+//   );
+// }
 
 export default function Search() {
   return (
-    <InstantSearchNext searchClient={client} indexName="instant_search" routing>
-      <div className="Container">
-        <div>
-          <DynamicWidgets fallbackComponent={FallbackComponent} />
-        </div>
-        <div>
-          <SearchBox />
-          <Hits hitComponent={Hit} />
-        </div>
-      </div>
-    </InstantSearchNext>
-  );
-}
-
-function FallbackComponent({ attribute }: { attribute: string }) {
-  return (
-    <Panel header={attribute}>
-      <RefinementList attribute={attribute} />
-    </Panel>
-  );
-}
-
-export function Panel({
-  children,
-  header,
-  footer,
-}: {
-  children: React.ReactNode;
-  header?: React.ReactNode;
-  footer?: React.ReactNode;
-}) {
-  return (
-    <div className="ais-Panel">
-      {header && <div className="ais-Panel-header">{header}</div>}
-      <div className="ais-Panel-body">{children}</div>
-      {footer && <div className="ais-Panel-footer">{footer}</div>}
+    <div>
+      <h1>TODO: BUILD THIS PAGE</h1>
     </div>
   );
 }
